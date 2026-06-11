b0c08a9702b78f0f27a6b0088083d3e3d6c13aaa
ruben-9091 <ruben_9091@hotmail.com>
2026-06-11 18:28:05 +0200

    Chore/projectscaffolding (#6)

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/home-page";
+import { NavBar } from "./components/ui";
+
+
+function App() {
+  return (
+  <>
+    <NavBar/>
+    <Routes>
+      <Route path="/" element={<HomePage/>}/>
+    </Routes>
+  </>
+  )
 function App() {
   return <></>;
 }
