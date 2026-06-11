dfb325d030fd52e1f0830e8a3b1c05386f5ba919
ruben-9091 <ruben_9091@hotmail.com>
2026-06-11 18:18:59 +0200

    feat: añadiendo mock

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import { worker } from "./mock/index.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>,
-)
+worker
+  .start({ onUnhandledRequest: "warn" })
+  .then(() => {
+    createRoot(document.getElementById("root")).render(
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>,
+    );
+  })
+  .catch((error) => console.error(error));
---
+++ src/mock/index.js
@@ -0,0 +1,19 @@
+import { setupWorker } from "msw/browser";
+import { HttpResponse, http } from "msw";
+import { heroes } from "./data-heroes";
+
+const baseMockURL = "http://api.heroes.mock.org";
+
+const handleHeroesList = http.get(`${baseMockURL}/characters`, () => {
+  return HttpResponse.json(heroes);
+});
+const handleCharacter = http.get(
+  `${baseMockURL}/characters/:id`,
+  ({ params }) => {
+    const hero = heroes.find((h) => h.id === Number(params.id));
+    if (!hero) return new HttpResponse(null, { status: 404 });
+    return HttpResponse.json(hero);
+  },
+);
+
+export const worker = setupWorker(handleHeroesList, handleCharacter);
