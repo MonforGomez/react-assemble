38df46555a3ea78c8a468a39b3c43510da752184
ruben-9091 <ruben_9091@hotmail.com>
2026-06-30 18:19:51 +0200

    fix: footer fix bug

--- src/components/layout/page-layout/page-layout.jsx
+++ src/components/layout/page-layout/page-layout.jsx
@@ -5,11 +5,13 @@ import { Footer, Jumbotron } from "../../ui";
 function PageLayout({ children, jumbotron, className = "" }) {
   return (
     <>
+    <div className="d-flex flex-column min-vh-100">
       {jumbotron && <Jumbotron {...jumbotron} />}
-      <div className={`container py-3 ${className}`}>
+      <div className={`container py-3 ${className} flex-grow-1` }>
         {children}
       </div>
       <Footer />
+      </div>
     </>
   );
 }
